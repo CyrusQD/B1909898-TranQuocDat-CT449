0aa1dc9268d7523cc3287ec2a1888184abcffc68
TranQuocDat_B1909898 <datb1909898@student.ctu.edu.vn>
2023-02-17 00:05:21 +0700

    Dinh nghia cac tai nguyen Contact

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const cors = require("cors");
 
 const app = express();
 
+const contactRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
 });
+
+module.exports = app;
+
+app.use("/api/contacts", contactRouter);
 
 module.exports = app;
